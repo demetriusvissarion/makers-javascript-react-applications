e494d88132758f6b20d15e01c7e2e545c483ee20
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-23 13:19:40 +0000

    add component

--- setting_up_project/src/App.jsx
+++ setting_up_project/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import Profile from "./Profile.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <Profile />;
+      </div>
     </>
   )
 }
---
+++ setting_up_project/src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Post = () => {
+    return (    
+    <div id="profile">
+        <h1 id="name">Quackie Makers</h1>
+
+        <p id="job">Makers' favourite rubber duck</p>
+        <p id="birthdate">2013</p>
+    </div>
+    );
+};
+
+export default Post;
